df8f1672fd9f546790fa872f78f99c2facdb0993
Rasmus Nielsen <rasmus@easify.dk>
2019-02-04 09:04:31 +0100

    Add docbloc properties to contract

--- src/Contracts/ResponsiveImage.php
+++ src/Contracts/ResponsiveImage.php
@@ -5,6 +5,12 @@ namespace Makeable\CloudImages\Contracts;
 use Makeable\CloudImages\ImageFactory;
 use Makeable\CloudImages\TinyPlaceholder;
 
+/**
+ * @property string $width
+ * @property string $height
+ * @property string $size
+ * @property string $tiny_placeholder
+ */
 interface ResponsiveImage
 {
     /**
