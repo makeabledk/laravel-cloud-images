1b6445a6028d099aaa194720caed2fc548206437
Rasmus Nielsen <rasmus@easify.dk>
2025-10-21 20:29:58 +0200

    Fix deprecation warning

--- src/ResponsiveImageFactory.php
+++ src/ResponsiveImageFactory.php
@@ -110,7 +110,7 @@ class ResponsiveImageFactory implements Arrayable, JsonSerializable
     /**
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
